f5f9d1915eb33b828a48dd221ae880e1408b727f
Abdulaziz Alshehri <me@abdulaziz-d.com>
2022-03-28 18:56:58 +0300

    New Features & Bug Fixes + Refactor

--- README.md
+++ README.md
@@ -10,8 +10,10 @@
 
     Scanning PHP Files for XSS Vulnerabilities Never Been Any Easier!
  ```    
+Current Version: 1.2.0
 
 -----
+
 
 
 ## Installation:
@@ -19,25 +21,52 @@
 1. `git clone https://github.com/bigzooooz/XSScanner`
 2. `cd XSScanner`
 3. `pip install requirements.txt`
-4. `python XSScanner.py -p <PATH>`
+4. `python XSScanner.py -d <PATH>`
 
 -----
 
 
 ## Usage:
+
+#### Copy the directory you want to scan into `SCAN` folder then provide path with the `-d` flag
+`python XSScanner.py -d SCAN/exampleDirectory`
+
+#### To Scan and Validate Vulnerablity Against Live Target
+
+`python XSScanner.py -d SCAN/exampleScript -t http://localhost/sameScript`
+
+*Output:*
+```
+[-] 3 Potential XSS Vulnerabilities Found.
+
+----------------------------------------
+[+] 200 - Vulnerable: http://localhost/sameScript/admin/index.php?page=XSS_PAYLOAD
+[+] 200 - Vulnerable: http://localhost/sameScript/admin/index.php?s=XSS_PAYLOAD
+----------------------------------------
+
+[-] Scan Completed!
+
+[+] Scan Result Saved in: Results/exampleScript/XSScanner-032822-182205-output.json
+
+```
+
+
+#### flags and usage help
+
 ```
 usage: XSScanner.py [-h] (-f FILE | -p PATH) [-o]
 
 options:
   -h, --help            show this help message and exit
-  -f FILE, --file FILE  Used To Define The Path of a Single File To Scan
-  -p PATH, --path PATH  Used To Define The Path of a Folder To Scan
+  -d PATH, --directory PATH  Used To Define The Path of a Folder To Scan
+  -t TARGET, --target TARGET Used To Define a Target URI To Test Against
   -o, --output          Print Scan Output on Screen (default=false)
+  --update              Update XSScanner
 ```
 
-All Results Will Be Stored in `XSS-{timestamp}-output.json` file.
+All Results Will Be Stored in `Results/{target_folder_name}/XSS-{timestamp}-output.json` file.
 
-Adding `-o` flag will print results on screen.
+Adding `-o` flag will STDOUT print results on screen.
 
 -----
 ## License
--- XSScanner.py
+++ XSScanner.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 
 from colorama import Fore
+from flask import jsonify
 from rich.console import Console
-import argparse, os, random, time, readline, json, datetime
+import requests, argparse, os, random, time, readline, json, datetime, re
+with open('VERSION', 'r', errors="ignore") as f:
+    version = f.readline()
 
-version = 0.1
-output_file = 'XSScanner-{}-output.json'.format(datetime.datetime.now().strftime("%x-%X").replace('/','').replace(':',''))
+def update_check():
+    try:
+        response = requests.get('https://raw.githubusercontent.com/bigzooooz/XSScanner/master/VERSION')
+        if response.status_code == 200:
+            if response.text != version:
+                print(Fore.MAGENTA + " New version available!")
+                time.sleep(1)
+                print(Fore.MAGENTA + " Add --update To Update")
+                print(Fore.GREEN + " Add --update To Update")
+                time.sleep(2)
+                return True
+            else:
+                return False
+    except:
+        return False
+    return False
+
 
 
 
@@ -73,19 +91,11 @@ def get_php_files_from_folder(folder_path):
         for file in files:
             if file.endswith(".php"):
                 php_files.append(os.path.join(root, file))
-    return scan_php_files_for_xss(php_files)
+    return php_files  
 
-def get_php_file(file):
-    if file.endswith(".php"):
-        php_file = [file]
-        return scan_php_files_for_xss(php_file)
-    print(Fore.RED + "File is not a PHP file")
-    exit()
-    
-            
-
-def scan_php_files_for_xss(files):
+def scan_php_files_for_xss(folder_path):
     console = Console()
+    files = get_php_files_from_folder(folder_path)
     tasks = [f"task {n}" for n in files]
     sources = ['GET','POST', 'REQUEST', "SERVER\['PHP" "SERVER\['PATH_" "SERVER\['REQUEST_U"]
     sinks = ['echo', 'die', 'print', 'printf', 'print_r', 'var_dump']
@@ -94,31 +104,111 @@ def scan_php_files_for_xss(files):
     with console.status("[bold green] Scanning {0} {1} ...".format(files_count, 'Files' if (files_count > 1) else 'File')) as status:
         time.sleep(2)
         for file_path in files:
+            scanned_path = file_path.split("SCAN")[-1]
             with open(file_path, 'r', errors="ignore") as f:
                 for n, line in enumerate(f, 1):
                     for source in sources:
                         if source in line:
                             for sink in sinks:
                                 if sink in line:
-                                    vulnerable.append({'file': file_path, 'line #': n, 'code': line.lstrip().rstrip() ,'source': source, 'sink': sink})
-                                    break
+                                    exploitable_files = locate_exploitable_file(file_path.split("/")[-1],files)
+                                    if(exploitable_files is None): break
+                                    exploit = []
+                                    for exploitable in exploitable_files:
+                                        exploitable_path = exploitable['file'].split("SCAN")[-1]
+                                        start = "$_"+source+"['"
+                                        end = "']"
+                                        parameter = line.lstrip().rstrip().partition(start)[2].partition(end)[0]
+                                        if source == "GET":
+                                            exploit.append({'file':exploitable_path.replace('/'+exploitable_path.split('/')[1],'').lstrip('/') +'?'+parameter+'=XSS_PAYLOAD', 'code':exploitable['code'], 'parameter':parameter})
+                                        else:
+                                            exploit.append({'file':exploitable_path.replace('/'+exploitable_path.split('/')[1],'').lstrip('/'), 'code':exploitable['code'], 'parameter':parameter})
+                                    vulnerable.append({'file': scanned_path, 'line #': n, 'code': line.lstrip().rstrip() ,'source': source, 'sink': sink, 'exploitation': exploit})
     return vulnerable
+
+def locate_exploitable_file(file_name,files):
+    console = Console()
+    exploitable = []
+    for file in files:
+        with open(file, 'r', errors="ignore") as f:
+            for n, line in enumerate(f, 1):
+                if(re.search(file_name,line) is not None):
+                    exploitable.append({'file': file, 'code': line.lstrip().rstrip()})
+    return exploitable
+   
+
+def scan_remote_target(target, vuln_list):
+    console = Console()
+    payload='XSS_PAYLOAD'
+    with console.status("[bold green] Scanning {0} for XSS ...".format(target)) as status:
+        time.sleep(2)
+        for v in vuln_list:
+            for exploit in v['exploitation']:
+                url = '{0}{1}'.format(target,exploit['file'])
+                http_status_errors = {
+                    400: "Bad Request",
+                    401: "Unauthorized",
+                    403: "Forbidden",
+                    404: "Not Found",
+                    405: "Method Not Allowed",
+                    408: "Request Timeout",
+                    500: "Internal Server Error",
+                    502: "Bad Gateway",
+                    503: "Service Unavailable",
+                    504: "Gateway Timeout",
+                }
+                try:
+                    if v['source'] == 'GET' or v['source'] == 'SERVER["PHP"]' or v['source'] == 'SERVER["PATH_"]' or v['source'] == 'SERVER["REQUEST_U"]' or v['source'] == 'REQUEST':
+                        response = requests.get(url)
+                    if response.status_code == 200:
+                        if payload in response.text:
+                            print(Fore.GREEN + "[+] {0} - Vulnerable: {1}".format(response.status_code,url))
+                    else:
+                        print(Fore.RED + "[!] {1} - {2} - Try Manually: {0}".format(url,response.status_code,http_status_errors[response.status_code]))
+                except requests.exceptions.ConnectionError:
+                    print(Fore.RED + "[!] DNS Error, Check your internet connection or target")
+                    return
 
 
 def main(argv):
+
+    if(argv['update']):
+        console = Console()
+        with console.status("Checking for Updates...") as status:
+            if(update_check()):
+                print(Fore.CYAN + "Current Version: {0}".format(version))
+                status.update(Fore.GREEN + "Update Found ...")
+                time.sleep(2)
+                status.update(Fore.GREEN + " Updating...")
+                print(Fore.GREEN + "[+]  Initiating git pull ...")
+                os.popen("git pull").read()
+                print(Fore.GREEN + "[+] Update Downloaded Successfully!")
+                print(Fore.GREEN + "[!] Please Restart ...")
+                status.update(Fore.GREEN + " Exiting...")
+                time.sleep(2)
+                exit()    
+            else:
+                print(Fore.MAGENTA + "[-] No Updates Available")
+                print(Fore.CYAN + "[-] Current Version: {0}".format(version))
+                time.sleep(2)
+                exit()
+
+    update_check()
+
+    if not os.path.exists('SCAN'):
+                    os.makedirs('SCAN')
     vulnerable = []
-    if (argv['path'] is not None):
-        folder_path = argv['path']
-        vulnerable = get_php_files_from_folder(folder_path)
+    folder_path = argv['directory']
+    output_folder = 'Results/{0}/'.format(folder_path.split("SCAN")[-1].lstrip('/').rstrip('/'))
+    output_file = 'Results/{0}/XSScanner-{1}-output.json'.format(folder_path.split("SCAN")[-1].lstrip('/').rstrip('/'),datetime.datetime.now().strftime("%x-%X").replace('/','').replace(':',''))
+    
+    if (argv['directory'] is not None):
+        vulnerable = scan_php_files_for_xss(folder_path)
         if vulnerable:
+            if not os.path.exists(output_folder):
+                os.makedirs(output_folder)
             with open(output_file, 'w+') as f:
-                    f.write(json.dumps(vulnerable) + '\n')
-    elif (argv['path'] is not None):
-        file_path = argv['path']
-        vulnerable = get_php_file(file_path)
-        if(len(vulnerable) > 1):
-            with open(output_file, 'w+') as f:
-                    f.write(json.dumps(vulnerable) + '\n')
+                    json.dump(vulnerable,f)
     print(Fore.RED + '\n[-] {} Potential XSS Vulnerabilities Found.\n'.format(len(vulnerable)))
     if(argv['output']):
         if vulnerable:
@@ -129,6 +219,13 @@ def main(argv):
                 print(Fore.RED + '\n[-] Code: ' + vuln['code'])
                 print(Fore.RED + '\n[-] Source: ' + vuln['source'])
                 print(Fore.RED + '\n[-] Sink: ' + vuln['sink'], end='\n\n')
+
+    if (argv['target'] is not None): 
+        target = argv['target']
+        if vulnerable:
+            print(Fore.RESET + '-' * 40,end='\n')
+            scan_remote_target(target, vulnerable)
+        
     
     print(Fore.RESET + '-' * 40,end='\n')
     print(Fore.GREEN + '\n[-] Scan Completed!')
@@ -148,10 +245,10 @@ if __name__ == "__main__":
     print(Fore.RESET)
 
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f','--file', help='Used To Define The Path of a Single File To Scan')
-    group.add_argument('-p','--path', help='Used To Define The Path of a Folder To Scan')
+    parser.add_argument('-d','--directory', help='Used To Define a Directory Path To Scan')
+    parser.add_argument('-t','--target', help='Used To Define a Target URI To Test Against', type=str)
     parser.add_argument("-o",'--output', help='Print Scan Output on Screen (default=false)', action='store_true',required=False, default=False)
+    parser.add_argument('--update', help='Update XSScanner', default=False, action='store_true')
     args = vars(parser.parse_args())
     readline.parse_and_bind('tab:complete')
     main(args)
